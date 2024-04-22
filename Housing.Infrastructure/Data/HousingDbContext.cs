e77970bdf407d6de0ecdd03a20f79ea7b6fe6c5b
ducna0610 <ducna0610@gmail.com>
2024-04-22 23:51:48 +0700

    feat: add DbSet

---
+++ Housing.Infrastructure/Data/HousingDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Housing.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Housing.Infrastructure.Data;
+
+public class HousingDbContext : DbContext
+{
+    public HousingDbContext(DbContextOptions<HousingDbContext> options) : base(options) { }
+
+    #region DbSet
+    DbSet<User> Users { get; set; }
+    DbSet<City> Cities { get; set; }
+    DbSet<Property> Properties { get; set; }
+    DbSet<Photo> Photos { get; set; }
+    #endregion
+}
