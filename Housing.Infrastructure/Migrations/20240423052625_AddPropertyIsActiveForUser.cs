a7cf231e49a8a9e42bc564641a458b7cbde81c27
ducna0610 <ducna0610@gmail.com>
2024-04-23 12:29:03 +0700

    feat: add property IsActive for User

---
+++ Housing.Infrastructure/Migrations/20240423052625_AddPropertyIsActiveForUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Housing.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPropertyIsActiveForUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsAvtive",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsAvtive",
+                table: "Users");
+        }
+    }
+}
